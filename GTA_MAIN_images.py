fe5d83d56a878d2c1a35f3f4ceef036c542ec827
YS <bomiiys@naver.com>
2021-11-22 12:44:13 +0900

    version 0.0.8

--- GTA_MAIN_images.py
+++ GTA_MAIN_images.py
@@ -4,16 +4,20 @@ import LineManage as lm
 import toArd
 import time
 
+# test by images
+from os import listdir 
+from os.path import isfile, join
+
 # ready to communicate with Arduino and Jetson
 connect = toArd.toArduino()
 # check connect
 connect.send(2,'0')
 
 # ready to get images
-video_path = -1
-video = cv2.VideoCapture(video_path)
-HEIGHT = 0
-WIDTH = 0
+# video_path = -1
+# video = cv2.VideoCapture(video_path)
+# HEIGHT = 0
+# WIDTH = 0
 
 # ready to get average steering and speed control
 speed_list = []
@@ -33,27 +37,55 @@ def get_averages(data_list):
 
 text = ''
 
+########################################################################
+# for test by images  ##################################################
+########################################################################
+cv2.namedWindow('img')
+mypath = 'images/1111'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+for i in range(len(onlyfiles)):
+    onlyfiles[i] = int(onlyfiles[i][:-4])
+
+sorted = sorted(onlyfiles)
+
+for i in range(len(onlyfiles)):
+    sorted[i] = str(sorted[i]) + '.jpg'
+
+images = np.empty(len(sorted), dtype=object)
+for n in range(0, len(sorted)):
+    images[n] = cv2.imread(join(mypath, sorted[n]))
+########################################################################
+# for test by images end ###############################################
+########################################################################
+
 cv2.namedWindow('img')
 count = 0
-while video.isOpened():
+#while video.isOpened():
+
+########################################################################
+# for test by images  ##################################################
+########################################################################
+for frame in images: 
     s_time = time.time()
-    ret, frame = video.read()
+    # ret, frame = video.read()
     if count == 0:
         HEIGHT, WIDTH = frame.shape[:2]
         print('width :', WIDTH, 'height :', HEIGHT)
 
-    if not ret:
-        break
+    # if not ret:
+    #     break
     
     # get average steering and speed
     data = lm.GetLine(frame)
     speed_list.append(data[0])
     steering_list.append(data[1])
 
-    if count % 5 == 0:
-        cv2.putText(frame, data, (100,50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3, cv2.LINE_AA)
-        cv2.imwrite('testimages/' + str(count) + '.jpg', frame)
-    if count != 0 and count % 30 == 0:
+    # if count % 5 == 0:
+    #     cv2.putText(frame, data, (100,50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3, cv2.LINE_AA)
+    #     cv2.imwrite('testimages/' + str(count) + '.jpg', frame)
+    #if count != 0 and count % 30 == 0:
+    if count > 0:    
         speed = get_averages(speed_list)
         steering = get_averages(steering_list)
 
@@ -68,16 +100,16 @@ while video.isOpened():
         speed_list = []
         steering_list = []
 
-        text = 'steering : ' + str(steering) + '\nspeed : ' + str(speed)
+        text = 'steering : ' + str(steering)
 
     fps = int(1 / (time.time() - s_time))
 
     cv2.putText(frame, str(fps), (100,150), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3, cv2.LINE_AA)
     cv2.putText(frame, text, (100,50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3, cv2.LINE_AA)
     count += 1
-    cv2.imshow('img', frame)
+    #cv2.imshow('img', frame)
 
-    input_key = cv2.waitKey(1) & 0xFF
+    input_key = cv2.waitKey(0) & 0xFF
     if  input_key == ord('q'):
         break
     elif input_key == ord(','):
